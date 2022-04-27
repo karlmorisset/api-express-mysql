a49caba3155898e3690e1474406871fdff620c3c
[WCS/NTS] Karl Morisset <90033508+karlmorisset@users.noreply.github.com>
2022-04-27 21:05:56 +0200

    Ajout du crud des heros

--- app.js
+++ app.js
@@ -9,6 +9,79 @@ const serverPort = process.env.PORT || 3000;
 // Nécessaire pour récupérer le body des requêtes POST et PUT
 app.use(express.json())
 
+
+app.get('/api/superheros', (req, res) => {
+  const { gender } = req.query;
+  let sql = 'SELECT * FROM heros';
+
+  const queryValues = [];
+  if (gender) {
+    sql += ' WHERE gender = ?';
+    queryValues.push(gender);
+  }
+
+  connection.promise().query(sql, queryValues)
+    .then(([data]) => {
+      res.json(data);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Erreur de récupération des heros.');
+    });
+});
+
+
+app.get('/api/superheros/:id', (req, res) => {
+  const { id } = req.params;
+
+  connection.promise()
+    .query('SELECT * FROM heros WHERE id = ?', [id])
+    .then(([data]) => {
+      if (data.length) {
+        res.json(data[0]);
+      } else {
+        res.sendStatus(404);
+      }
+    });
+});
+
+app.post('/api/superheros', (req, res) => {
+  const { name, gender, power, color } = req.body;
+
+  connection.promise()
+    .query('INSERT INTO heros (name, gender, power, color) VALUES (?,?,?,?)',
+    [name, gender, power, color])
+    .then(([data]) => {
+      const createdHero = {id: data.insertId, name, gender, power, color};
+      res.status(201).json(createdHero);
+    }).catch((err) => { console.error(err); res.sendStatus(500); });
+});
+
+app.put('/api/superheros/:id', (req, res) => {
+  connection.promise()
+    .query('UPDATE heros SET ? WHERE id = ?', [req.body, req.params.id])
+    .then(() =>  res.sendStatus(200))
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+});
+
+app.delete('/api/superheros/:id', (req, res) => {
+  connection.promise()
+    .query('DELETE FROM heros WHERE id = ?', [req.params.id])
+    .then(([data]) => {
+      if (data.affectedRows) res.sendStatus(204);
+      else res.sendStatus(404);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+});
+
+
+
 // Connexion à la base de données
 connection.connect((err) => {
   if (err) {
